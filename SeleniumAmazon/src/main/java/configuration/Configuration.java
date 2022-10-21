06ff10efc0b51875dac0fbfc04c7b8d98b7660ff
Andy25a <57402172+Andy25a@users.noreply.github.com>
2022-10-21 11:34:21 -0500

    Delete SeleniumAmazon directory

--- SeleniumAmazon/src/main/java/configuration/Configuration.java
+++
@@ -1,9 +0,0 @@
-package configuration;
-
-public class Configuration {
- 
-	  public static final String CHROME_BROWSER="webdriver.chrome.driver";
-	  public static final String CHROME_DRIVER="./src/test/resources/webdriver/chromedriver.exe";
-	  public static final String BASE_URL = "https://www.amazon.com/";
-	  
-}
