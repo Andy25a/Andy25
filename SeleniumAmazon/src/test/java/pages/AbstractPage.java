0860bb24065bb7714f28171493b434e5642867ef
Andy25a <57402172+Andy25a@users.noreply.github.com>
2022-10-21 11:23:28 -0500

    Add files via upload

---
+++ SeleniumAmazon/src/test/java/pages/AbstractPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public abstract class AbstractPage {
+
+    protected static WebDriver driver;
+
+    public AbstractPage(WebDriver driver) {
+        AbstractPage.driver = driver;
+    }
+}
